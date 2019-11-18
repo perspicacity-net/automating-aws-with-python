af185cc0098829c1aa4b980a12c10f9ac53637fb
perspicacity-net <57500925+perspicacity-net@users.noreply.github.com>
2019-11-18 15:00:15 -0500

    Fix locationconstraint for us-east-1

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -4,7 +4,19 @@ from botocore.exceptions import ClientError
 
 session = boto3.Session(profile_name='PythonAutomation')
 s3 = session.resource('s3')
-@@ -22,5 +23,52 @@ def list_bucket_objects(bucket):
+@click.group()
+def cli():
+    "Webotron deploys websites to AWS"
+    pass
+@cli.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List objects in an s3 bucket"
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
 
@@ -16,7 +28,7 @@ def setup_bucket(bucket):
     try:
         s3_bucket = s3.create_bucket(
             Bucket=bucket,
-            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+            ## CreateBucketConfiguration={'LocationConstraint': session.region_name}
         )
     except ClientError as e:
         if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
